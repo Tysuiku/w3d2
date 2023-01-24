079b7fb4e1789826d31eab6c01a5f1ade6aaaa8b
Tysuiku <Timothydong41@gmail.com>
2023-01-24 14:14:07 -0500

    added some methods to card class

--- memory_puzzle/card.rb
+++ memory_puzzle/card.rb
@@ -1,2 +1,20 @@
 class Card
+  attr_reader :face_value, :face_side
+
+  def initialize
+    @face_value = face_value
+    @face_side = false
+  end
+
+  def hide
+    return @face_value == "" if !@face_side
+  end
+
+  def reveal
+    return @face_value unless @face_side
+  end
+
+  def to_s
+    @face_value.to_s
+  end
 end
