c3bef9fde13ec05c5eae4a730e061396ee6302d5
Tysuiku <Timothydong41@gmail.com>
2023-01-24 15:15:32 -0500

    added functions to help populate board with pairs

--- memory_puzzle/board.rb
+++ memory_puzzle/board.rb
@@ -1,27 +1,41 @@
 require_relative "card.rb"
 
 class Board
+  attr_reader :size
 
-    def initialize(size)
+  def initialize(size)
     raise "Please pick an even number" if size % 2 != 0
-    @grid =  Array.new(size){Array.new(size)}
-   
+    @size = size
+    @grid = Array.new(size) { Array.new(size) }
+    @card = Card.new
+  end
 
-    end
-
-    def [](pos)
-
-    row,col = pos
+  def [](pos)
+    row, col = pos
 
     @grid[row][col]
-    
-    end
+  end
 
-    def []=(pos,mark)
-
-    row,col = pos
+  def []=(pos, mark)
+    row, col = pos
 
     @grid[row][col] = mark
+  end
 
+  def pairs
+    pair = []
+    rand = @card.face_value
+    2.times do
+      pair << rand
     end
+
+    pair
+  end
+
+  def populate
+    pairs = []
+    @size.times do
+      pairs += self.pair
+    end
+  end
 end
--- memory_puzzle/card.rb
+++ memory_puzzle/card.rb
@@ -1,25 +1,17 @@
 class Card
   attr_reader :face_value, :face_side, :face_down_value
 
-  
-
   def initialize
     @face_value = ("a".."z").to_a.sample
     @face_down = true
     @face_down_value = ""
   end
 
-  
-
   def hide
     return @face_down_value unless @face_down
   end
 
   def reveal
-    return @face_value  
+    return @face_value
   end
-
-  
-
-
 end
